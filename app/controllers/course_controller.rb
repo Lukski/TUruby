cdf0f825f8334c71c9c70566b4c598a86c5ae1ed
Patrick <patrick@hromniak.at>
2018-06-15 17:29:04 +0200

    Thesis / Course UI

--- app/controllers/course_controller.rb
+++ app/controllers/course_controller.rb
@@ -2,4 +2,8 @@ class CourseController < SearchableController
   def get_search(query)
     "https://tiss.tuwien.ac.at/api/search/course/v1.0/quickSearch?searchterm=#{query}"
   end
+
+  def get_detail_url(id)
+    "https://tiss.tuwien.ac.at/api/thesis/#{id}"
+  end
 end

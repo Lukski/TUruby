a0ca102a8a4b69ec1057a5138e3242a4795d8b6b
Patrick <patrick@hromniak.at>
2018-06-17 10:23:51 +0200

    View Projects

---
+++ app/controllers/project_controller.rb
@@ -0,0 +1,20 @@
+class ProjectController < SearchableController
+
+  def self.get_search(query)
+    "https://tiss.tuwien.ac.at/api/search/projectFullSearch/v1.0/projects?searchterm=#{query}"
+  end
+
+  def self.get_detail_url(id)
+    "https://tiss.tuwien.ac.at/api/pdb/rest/project/v2/#{id}"
+  end
+
+  def self.object_type()
+    :project
+  end
+
+  def self.get_project_id_from_url(url)
+    x = url.split("=")
+    return x[1]
+  end
+
+end
